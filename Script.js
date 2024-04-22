9daa87d197da3485a8338935c0fe5aecc5d8703d
Luis Eduardo <eu.luiseduardo.pro@gmail.com>
2024-04-22 16:43:58 -0300

    alertas

--- Script.js
+++ Script.js
@@ -8,6 +8,10 @@ const botoes = document.querySelectorAll('.app__card-button')
 const startpauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioplay = new Audio ('/sons/play.wav')
+const audiopause = new Audio ('/sons/pause.mp3')
+const audioend = new Audio ('/sons/beep.mp3')
+
 
 let intervaloid = null
 let tempodecorridoemsegundos = 5
@@ -62,9 +66,11 @@ function alterarContexto(contexto){
 
 const contagemregressiva = () =>{
     if(tempodecorridoemsegundos<=0){
+        audioend.play()
         zerar()
         alert ('tempo finalizado')
         return 
+
     } 
     tempodecorridoemsegundos -=1
     console.log('temporizador: ' + tempodecorridoemsegundos)
@@ -77,10 +83,14 @@ function iniciaroupausar() {
         // If intervaloid is already set (meaning the timer is running), pause the timer
         clearInterval(intervaloid); // Clear the interval to pause the countdown
         intervaloid = null; // Reset intervaloid to null to indicate that the timer is paused
+        audiopause.play()
     } else {
         // If intervaloid is not set (meaning the timer is not running), start the timer
         intervaloid = setInterval(contagemregressiva, 1000); // Start the countdown timer
+    audioplay.play()
+    
     }
+
 }
 function zerar(){
     clearInterval(intervaloid)
